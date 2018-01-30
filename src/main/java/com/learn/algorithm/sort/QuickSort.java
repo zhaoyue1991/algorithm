d0122a85f69163c5c83d3338ef1894f93e0d7ee2
zhaoyue <zhaoyue@glzc.com>
2018-01-30 14:37:22 +0800

    add quick_sort

---
+++ src/main/java/com/learn/algorithm/sort/QuickSort.java
@@ -0,0 +1,46 @@
+package com.learn.algorithm.sort;
+
+import java.util.Arrays;
+
+public class QuickSort {
+
+  /**
+   * @param a 原数组
+   * @param p 开始下标
+   * @param r 结束下标
+   */
+  private static void quickSort(int[] a, int p, int r) {
+    if (p < r) {
+      int q = partition(a, p, r);
+      quickSort(a, p, q - 1);
+      quickSort(a, q + 1, r);
+    }
+  }
+
+  /**
+   * @param a 原始数组
+   * @param p 开始下标
+   * @param r 结束下标
+   */
+  private static int partition(int[] a, int p, int r) {
+    int x = a[r];
+    int i = p - 1;
+    for (int j = p; j < r; j++) {
+      if (a[j] < x) {
+        i = i + 1;
+        int temp = a[i];
+        a[i] = a[j];
+        a[j] = temp;
+      }
+    }
+    a[r] = a[i + 1];
+    a[i + 1] = x;
+    return i + 1;
+  }
+
+  public static void main(String[] args) {
+    int[] a = {2, 8, 7, 1, 3, 5, 6, 4};
+    quickSort(a, 0, a.length - 1);
+    System.out.println(Arrays.toString(a));
+  }
+}
