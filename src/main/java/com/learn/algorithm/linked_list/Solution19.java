91c40976315441aca2e21070bc6e8f54027cd2c7
zhaoyue23 <zhaoyue23@58.com>
2020-12-15 14:50:32 +0800

    单链表

--- src/main/java/com/learn/algorithm/linked_list/Solution19.java
+++ src/main/java/com/learn/algorithm/linked_list/Solution19.java
@@ -7,15 +7,39 @@ package com.learn.algorithm.linked_list;
  */
 public class Solution19 {
 
-//    public ListNode removeNthFromEnd(ListNode head, int n) {
-//        int temp = 0;
-//    }
-//
-//    public ListNode help(ListNode head, int n) {
-//        if(head == null){
-//            r
-//        }
-//    }
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        if (null == head) {
+            return null;
+        }
+        ListNode slow = head;
+        ListNode prev = new ListNode(-1);
+        prev.next = slow;
+        ListNode fast = findFast(head, n);
+        if (fast == null) {
+            // 删除第一个节点
+            prev.next = slow.next;
+            return prev.next;
+        }
+        while (fast.next != null) {
+            fast = fast.next;
+            slow = slow.next;
+        }
+        slow.next = slow.next.next;
+        return prev.next;
+    }
+
+    public ListNode findFast(ListNode head, int n) {
+        int temp = 0;
+        while (head != null) {
+            temp++;
+            if (temp == n) {
+                return head.next;
+            } else {
+                head = head.next;
+            }
+        }
+        return null;
+    }
 
     public class ListNode {
         int val;
